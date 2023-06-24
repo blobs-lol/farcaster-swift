f62e7b909aa6a59d1675246fa2f534871e7f68c1
Michael Huang <michaelhly@gmail.com>
2023-06-23 21:25:30 -0400

    GRPC Client builders (#1)

--- Package.swift
+++ Package.swift
@@ -13,12 +13,19 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.15.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Farcaster"),
+            name: "Farcaster",
+            dependencies: [
+                .product(name: "GRPC", package: "grpc-swift"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+            ],
+            path: "Sources",
+            sources: ["HubServiceClient", "Generated"]),
         .testTarget(
             name: "FarcasterTests",
             dependencies: ["Farcaster"]),
---
+++ Sources/HubServiceClient/Builders.swift
@@ -0,0 +1,139 @@
+//
+//  ClientBuilder.swift
+//  farcaster-swift
+//
+//  Created by Michael Huang on 6/23/23.
+//
+
+import GRPC
+import NIO
+
+enum ValidationError: Error {
+    case invalidAddress(address: String)
+    case portNotFound
+}
+
+class HubServiceClientBuilder {
+    private let cfg: GRPCChannelPool.Configuration
+    private var callOptions: CallOptions
+    private var interceptors: HubServiceClientInterceptorFactoryProtocol?
+
+    init(cfg: GRPCChannelPool.Configuration,
+         defaultCallOptions: CallOptions = CallOptions(),
+         interceptors: HubServiceClientInterceptorFactoryProtocol? = nil
+    ) {
+        self.cfg = cfg
+        self.callOptions = defaultCallOptions
+        self.interceptors = interceptors
+    }
+
+    static func makeSecureClient(
+        address: String,
+        transportSecurity: GRPCChannelPool.Configuration.TransportSecurity,
+        eventLoopGroup: EventLoopGroup
+    ) throws -> HubServiceClientProtocol {
+        let (host, port) = try getHostAndPort(address: address)
+        return try HubServiceClientBuilder.init(cfg: .with(
+            target: .host(host, port: port),
+            transportSecurity: transportSecurity,
+            eventLoopGroup: eventLoopGroup
+        )).build()
+    }
+
+    static func makeInsecureClient(
+        address: String,
+        eventLoopGroup: EventLoopGroup
+    ) throws -> HubServiceClientProtocol {
+        let (host, port) = try getHostAndPort(address: address)
+        return try HubServiceClientBuilder.init(cfg: .with(
+            target: .host(host, port: port),
+            transportSecurity: .plaintext,
+            eventLoopGroup: eventLoopGroup
+        )).build()
+    }
+
+    func withInterceptors(interceptors: HubServiceClientInterceptorFactoryProtocol) {
+        self.interceptors = interceptors
+    }
+
+    func withCallOptions(callOptions: CallOptions) {
+        self.callOptions = callOptions
+    }
+
+    func build() throws -> HubServiceClientProtocol {
+        return HubServiceNIOClient(
+            channel: try GRPCChannelPool.with(configuration: cfg),
+            defaultCallOptions: callOptions,
+            interceptors: interceptors
+        )
+    }
+}
+
+@available(macOS 10.15, *)
+class HubServiceAsyncClientBuilder {
+    private let cfg: GRPCChannelPool.Configuration
+    private var callOptions: CallOptions
+    private var interceptors: HubServiceClientInterceptorFactoryProtocol?
+
+    init(cfg: GRPCChannelPool.Configuration,
+         defaultCallOptions: CallOptions = CallOptions(),
+         interceptors: HubServiceClientInterceptorFactoryProtocol? = nil
+    ) {
+        self.cfg = cfg
+        self.callOptions = defaultCallOptions
+        self.interceptors = interceptors
+    }
+
+    static func makeSecureClient(
+        address: String,
+        transportSecurity: GRPCChannelPool.Configuration.TransportSecurity,
+        eventLoopGroup: EventLoopGroup
+    ) throws -> HubServiceAsyncClientProtocol {
+        let (host, port) = try getHostAndPort(address: address)
+        return try HubServiceAsyncClientBuilder.init(cfg: .with(
+            target: .host(host, port: port),
+            transportSecurity: transportSecurity,
+            eventLoopGroup: eventLoopGroup
+        )).build()
+    }
+
+    static func makeInsecureClient(
+        address: String,
+        eventLoopGroup: EventLoopGroup
+    ) throws -> HubServiceAsyncClientProtocol {
+        let (host, port) = try getHostAndPort(address: address)
+        return try HubServiceAsyncClientBuilder.init(cfg: .with(
+            target: .host(host, port: port),
+            transportSecurity: .plaintext,
+            eventLoopGroup: eventLoopGroup
+        )).build()
+    }
+
+    func withInterceptors(interceptors: HubServiceClientInterceptorFactoryProtocol) {
+        self.interceptors = interceptors
+    }
+
+    func withCallOptions(callOptions: CallOptions) {
+        self.callOptions = callOptions
+    }
+
+    func build() throws -> HubServiceAsyncClientProtocol {
+        return HubServiceAsyncClient(
+            channel: try GRPCChannelPool.with(configuration: cfg),
+            defaultCallOptions: callOptions,
+            interceptors: interceptors
+        )
+    }
+}
+
+private func getHostAndPort(address: String) throws -> (String, Int) {
+    let parts = address.components(separatedBy: ":")
+    if parts.count != 2 {
+        throw ValidationError.invalidAddress(address: address)
+    }
+    let host = parts[0]
+    guard let port = Int(parts[1]) else {
+        throw ValidationError.portNotFound
+    }
+    return (host, port)
+}
